85e8bedc16473facefbd3c34f00302447fc8f3fc
AnsellC <ansellcruz@gmail.com>
2014-02-26 16:09:12 +0800

    version number

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -6,5 +6,9 @@
 		<li><a href="/contact">Contact Us</a></li>
 		<li><a href="/api">API</a></li>
 	</ul>
-	<span class="version">{{ file_get_contents(base_path() . "/version") }}</span>
+	<span class="version">
+		@if(file_exists(base_path() . "/version"))
+			{{ file_get_contents(base_path() . "/version") }}
+		@endif
+	</span>
 </footer>
