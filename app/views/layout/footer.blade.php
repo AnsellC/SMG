cf5e4b4fb6c71e9a26bdb0a0690ea38459c16ed2
AnsellC <ansellcruz@gmail.com>
2014-02-26 15:35:25 +0800

    fixed version

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -6,5 +6,5 @@
 		<li><a href="/contact">Contact Us</a></li>
 		<li><a href="/api">API</a></li>
 	</ul>
-	<span class="version">{{ exec("git describe --long") }}</span>
+	<span class="version">{{ file_get_contents(base_path() . "/version") }}</span>
 </footer>
