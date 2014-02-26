d7c9fc9a926ebb077879e3f31d2f46322360ecfa
AnsellC <ansellcruz@gmail.com>
2014-02-26 15:41:04 +0800

    f

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -6,5 +6,5 @@
 		<li><a href="/contact">Contact Us</a></li>
 		<li><a href="/api">API</a></li>
 	</ul>
-	<span class="version">{{ file_get_contents(base_path() . "/version") }}</span>
+	<span class="version">{{ file_get_contents(base_path() . "/version")   }}</span>
 </footer>
